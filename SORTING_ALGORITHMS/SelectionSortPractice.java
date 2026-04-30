808c062124ef40637a36a98857617b7e47ab7d50
Debashis Halder <debashishalder185@gmail.com>
2026-04-30 22:57:31 +0530

    Add files via upload

---
+++ SORTING_ALGORITHMS/SelectionSortPractice.java
@@ -0,0 +1,25 @@
+public class SelectionSortPractice {
+    public static void selectionSort(int arr[]) {
+        for(int i=0; i<arr.length-1; i++) {
+            int minPos = i;
+            for(int j=i+1; j<arr.length; j++) {
+                if(arr[minPos]> arr[j]) {
+                    minPos = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[minPos];
+            arr[minPos] = temp;
+        } 
+    }
+    public static void printArray(int arr[]) {
+        for(int i=0; i<arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+    }
+    public static void main(String args[]) {
+        int arr[] = {3, 6, 2, 1, 8, 7, 4, 5, 3, 1};
+        selectionSort(arr);
+        printArray(arr);
+    }
+}
